13cf0b468455bf6b7f46ba97b2a052f2cb806ac2
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-08-28 14:06:00 +0530

    longlasting commit

--- index.js
+++ index.js
@@ -10,18 +10,20 @@ const app =express()
 
 app.use(cors(
          {
-    origin:["https://car-rental-front-end-mohanlal.vercel.app","http://localhost:5173"], 
+    origin:["https://car-rental-front-end-mohanlal.vercel.app","http://localhost:5173"],  
     
-     methods:['GET','POST','PUT','DELETE','OPTIONS'],
+     methods:['GET','POST','PUT','DELETE','OPTIONS'], 
      allowedHeaders:['Content-Type', 'Authorization'], 
      credentials : true, 
 }
-)) 
+)
+); 
 app.use(express.json())
 app.use(cookieParser())
 app.get("/",(req,res)=>{  
     res.send("hello world")
-})
+}
+);
 
 app.use("/api",apiRouter)
 
