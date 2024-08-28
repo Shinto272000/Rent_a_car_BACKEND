7a5396268df9e67027a6e03ceb6fc3de9716c915
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-08-28 11:40:10 +0530

    achama commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app =express()
 
 app.use(cors(
          {
-    origin:["https://car-rental-front-end-secodhooope.vercel.app"], 
+    origin:["https://car-rental-front-end-secodhooope.vercel.app","http://localhost:5173"], 
     credentials : true, 
     //  methods: 'GET,POST,PUT,DELETE,OPTIONS', 
 }
