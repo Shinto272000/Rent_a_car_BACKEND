536e74a6a5940e00d59851c8f49b4f544b54aa45
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-08-30 20:15:12 +0530

    hopy commit

--- Src/Controllers/userControllers.js
+++ Src/Controllers/userControllers.js
@@ -97,7 +97,7 @@ const signin = async (req, res) => {
     const isProduction = process.env.NODE_ENV === "production";
         // console.log(isProduction,'====idProduction');
         
-        res.cookie("token", token, {
+        res.cookie("tokenssss", token, {
             maxAge: 24 * 60 * 60 * 1000, // 1 day
             httpOnly: true,
             secure: isProduction, // Secure only in production
