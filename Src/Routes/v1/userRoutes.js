40e3431d5059bd8f456eed6ad116272f853edebb
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-08-30 19:17:57 +0530

    check user commit

--- Src/Routes/v1/userRoutes.js
+++ Src/Routes/v1/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import userController from "../../Controllers/userControllers.js";
 import carController from "../../Controllers/carControllers.js";
+import authenticateUser from "../../Middlewares/userMiddleware.js";
 const userRouter = express.Router()
 
 userRouter.get("/",(req,res)=>{
@@ -11,7 +12,7 @@ userRouter.post("/signup",userController.signup)
 userRouter.post("/signin",userController.signin)
 userRouter.get("/all-cars",carController.getCar)
 userRouter.get("/all-cars/:id",carController.getoneCar)
-userRouter.get("/check-user",userController.checkUser)
+userRouter.get("/check-user",authenticateUser,userController.checkUser)
 
 
 export default userRouter 
