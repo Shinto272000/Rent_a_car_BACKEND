5c7ee0e44228d930e2044c2eeffda92e132883b6
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-09-04 17:34:12 +0530

    Update userMiddleware.js

--- Src/Middlewares/userMiddleware.js
+++ Src/Middlewares/userMiddleware.js
@@ -4,12 +4,12 @@ import serverConfig from "../Config/serverConfig.js";
 
 function authenticateUser(req, res, next){
 
-    // const token = req.sessionStorage.token;
-    const token = req.cookies.token;  
+    const token = req.sessionStorage.token;
+    // const token = req.cookies.token;   
     jwt.verify(token,serverConfig.token, (err, user)=>{
         if (err){ 
             console.log(err); 
-            return res.status(401).send("not verified") 
+            return res.status(401).send("not verified")  
         }
 
         // console.log("user token result",result);
