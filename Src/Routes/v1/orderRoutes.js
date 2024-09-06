4fb41da1342fb8165b8ae58cda8ab852f13a18d3
Shinto2707 <151459643+Shinto2707@users.noreply.github.com>
2024-09-06 18:59:37 +0530

    vasudeva commit

--- Src/Routes/v1/orderRoutes.js
+++ Src/Routes/v1/orderRoutes.js
@@ -7,5 +7,5 @@ const orderRouter = express.Router()
 orderRouter.post("/details",ordercontroller.orderdatas) 
 orderRouter.get("/testing",ordercontroller.testingg)
 orderRouter.get('/orders/:userId',ordercontroller.userorder);
-orderRouter.get('/allorders/',ordercontroller.getallorder);
+orderRouter.get('/allorders',ordercontroller.getallorder);
 export default orderRouter
